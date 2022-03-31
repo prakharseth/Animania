b6ed5b5464b44c5c8c0a822561d212b785578916
prakharseth <56907863+prakharseth@users.noreply.github.com>
2022-03-31 07:14:56 +0530

    first-commit(desktop)
    In the progress

---
+++ README.md
@@ -0,0 +1,4 @@
+## Hey there, I am Prakhar Seth a front-end developer and my journey is to be on the way to become a full stack web developer
+## In this project first I design this project there after I convert it in the website
+
+# Thanks for visiting here
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fira: ["Fira Sans", "sans-serif"],
+        rock: ["RocknRoll One", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
